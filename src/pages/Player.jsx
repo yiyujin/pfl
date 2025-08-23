10538c0bdec950a91fd387fd3d7e601475bccab7
Jin <40701844+yiyujin@users.noreply.github.com>
2025-08-23 01:33:37 -0400

    databind stats to each player

--- src/pages/Player.jsx
+++ src/pages/Player.jsx
@@ -6,11 +6,40 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger';
 gsap.registerPlugin(ScrollTrigger);
 
 import YoutubePlayer from '../components/YoutubePlayer';
-
+import RandomText from "../components/RandomText";
 import { ArrowForward, EventAvailable, Stadium, Scoreboard } from '@mui/icons-material';
+import { dataPlayers } from "../data/dataPlayers";
+import { dataTeams } from "../data/dataTeams";
+import { dataStats } from "../data/dataStats";
 
 export default function Player(){
     const { id } = useParams(); // player_id
+
+    // PARSE DATA
+    const data = dataPlayers.results.filter( (item) => item.id === id );
+    const playerName = data[0].properties.Name.title[0].plain_text;
+    const backNumber = data[0].properties.back_number.rich_text[0].plain_text;
+
+    const teamId = data[0].properties.Teams.relation[0].id;
+    const team = dataTeams.results.filter( (item) => item.id === teamId );
+    const teamName = team[0].properties.Name.title[0].plain_text;
+
+    const statsArr = dataStats.results.filter( (item) => (item.properties.player.relation[0].id) === id );
+    
+    const stats = {
+        "GP" : String(statsArr[0].properties.GP.rich_text[0].plain_text),
+        "MIN" : String(statsArr[0].properties.MIN.rich_text[0].plain_text),
+        "PTS" : String(statsArr[0].properties.PTS.formula.number),
+        "FG" : String(statsArr[0].properties.FG.number),
+        "FGA" : String(statsArr[0].properties.FGA.number),
+        "FGP" : String(statsArr[0].properties.FGP.formula.number),
+        "3FG" : String(statsArr[0].properties["3FG"].number),
+        "3FGA" : String(statsArr[0].properties["3FGA"].number),
+        "3FGP" : String(statsArr[0].properties["3FGP"].formula.number),
+        "FT" : String(statsArr[0].properties.FT.number),
+        "FTA" : String(statsArr[0].properties.FTA.number),
+        "FTP" : String(statsArr[0].properties.FTP.formula.number),
+    }
 
     const statsContainerRef = useRef(null);
     const spanRefs = useRef([]);
@@ -76,8 +105,8 @@ export default function Player(){
     return(
         <div className = "page">
             <div style = { { } }>
-                <h1>22 · James Smith</h1>
-                <p className = "meta">Team : Clark Cougars</p>
+                <h1>{ backNumber } · { playerName }</h1>
+                <p className = "meta">Team : { teamName }</p>
             </div>
 
             <br/>
@@ -99,21 +128,21 @@ export default function Player(){
                 {/* NUMBER STATS */}
                 <div style = { {  display : "flex", flexDirection : "column", alignItems : "center", justifyContent : "center", gap : "8px" } }>
                     <div style = { { border : "1px solid transparent", width : "160px", height : "180px", display : "flex", flexDirection : "column", alignItems : "center", justifyContent : "center" } }>
-                        <h3 className = "stats-number" style = { { fontSize : "100px" } }>8</h3>
+                        <RandomText txt = { stats.GP } fontSize = "100"/>
                     </div>
                     <p className = "meta">Games Played</p>
                 </div>
 
                 <div style = { {  display : "flex", flexDirection : "column", alignItems : "center", justifyContent : "center", gap : "8px" } }>
                     <div style = { { border : "1px solid transparent", width : "160px", height : "180px", display : "flex", flexDirection : "column", alignItems : "center", justifyContent : "center" } }>
-                        <h3 className = "stats-number" style = { { fontSize : "100px" } }>243</h3>
+                        <RandomText txt = { stats.MIN } fontSize = "100"/>
                     </div>
                     <p className = "meta">Min Played</p>
                 </div>
 
-                                <div style = { {  display : "flex", flexDirection : "column", alignItems : "center", justifyContent : "center", gap : "8px" } }>
+                <div style = { {  display : "flex", flexDirection : "column", alignItems : "center", justifyContent : "center", gap : "8px" } }>
                     <div style = { { border : "1px solid transparent", width : "160px", height : "180px", display : "flex", flexDirection : "column", alignItems : "center", justifyContent : "center" } }>
-                        <h3 className = "stats-number" style = { { fontSize : "100px" } }>92</h3>
+                        <RandomText txt = { stats.PTS } fontSize = "100"/>
                     </div>
                     <p className = "meta">Pts</p>
                 </div>
@@ -123,17 +152,17 @@ export default function Player(){
                 <div style = { { display : "flex", flexDirection : "column", alignItems : "center", width : "100%", gap : "8px" } }>
                     <div style={{ display: "flex", flexDirection: "row", width: "320px", height: "180px", background: "", border: "1px solid white" }}>
                         <div style={{ background: "", display: "flex", flexDirection: "column", flex: 1, borderRight: "1px solid white" }}>
-                            <h3 className = "stats-number" style={{ flex: 1, display: "flex", justifyContent: "center", alignItems: "center", borderBottom: "1px solid white" }}>41</h3>
-                            <h3 className = "stats-number" style={{ flex: 1, display: "flex", justifyContent: "center", alignItems: "center" }}>90</h3>
+                            <h3 className = "stats-number" style={{ flex: 1, display: "flex", justifyContent: "center", alignItems: "center", borderBottom: "1px solid white" }}>{ stats.FG }</h3>
+                            <h3 className = "stats-number" style={{ flex: 1, display: "flex", justifyContent: "center", alignItems: "center" }}>{ stats.FGA }</h3>
                         </div>
                         <div style={{ position : "relative", flex: 1, display: "flex", justifyContent: "center", alignItems: "center" }}>
                             <div style = { { display : "flex", flexDirection : "row", alignItems : "flex-end"} }>
-                                <h3 className = "stats-number" style = { { fontSize : "100px" } }>46</h3>
+                                <h3 className = "stats-number" style = { { fontSize : "100px" } }>{ stats.FGP }</h3>
                                 <p className = "percentage">%</p>
                             </div>
                             <span 
                                 ref={(el) => spanRefs.current[0] = el}
-                                data-target-height="46%"
+                                data-target-height = {`${stats.FGP}%`}
                                 style = { { bottom : "0", position : "absolute", width : "100%", height : "0%", background: "white", mixBlendMode: "difference", }}
                             />
                         </div>
@@ -145,17 +174,17 @@ export default function Player(){
                 <div style = { { display : "flex", flexDirection : "column", alignItems : "center", width : "100%", gap : "8px" } }>
                     <div style={{ display: "flex", flexDirection: "row", width: "320px", height: "180px", background: "", border: "1px solid white" }}>
                         <div style={{ background: "", display: "flex", flexDirection: "column", flex: 1, borderRight: "1px solid white" }}>
-                            <h3 className = "stats-number" style={{ flex: 1, display: "flex", justifyContent: "center", alignItems: "center", borderBottom: "1px solid white" }}>5</h3>
-                            <h3 className = "stats-number" style={{ flex: 1, display: "flex", justifyContent: "center", alignItems: "center" }}>20</h3>
+                            <h3 className = "stats-number" style={{ flex: 1, display: "flex", justifyContent: "center", alignItems: "center", borderBottom: "1px solid white" }}>{ stats["3FG"] }</h3>
+                            <h3 className = "stats-number" style={{ flex: 1, display: "flex", justifyContent: "center", alignItems: "center" }}>{ stats["3FGA"] }</h3>
                         </div>
                         <div style={{ position : "relative", flex: 1, display: "flex", justifyContent: "center", alignItems: "center" }}>
                             <div style = { { display : "flex", flexDirection : "row", alignItems : "flex-end"} }>
-                                <h3 className = "stats-number" style = { { fontSize : "100px" } }>25</h3>
+                                <h3 className = "stats-number" style = { { fontSize : "100px" } }>{ stats["3FGP"] }</h3>
                                 <p className = "percentage">%</p>
                             </div>
                             <span 
                                 ref={(el) => spanRefs.current[1] = el}
-                                data-target-height="22%"
+                                data-target-height = {`${stats["3FGP"]}%`}
                                 style = { { bottom : "0", position : "absolute", width : "100%", height : "0%", background: "white", mixBlendMode: "difference", }}
                             />
                         </div>
@@ -167,17 +196,17 @@ export default function Player(){
                 <div style = { { marginRight : "80px", display : "flex", flexDirection : "column", alignItems : "center", width : "100%", gap : "8px" } }>
                     <div style={{ display: "flex", flexDirection: "row", width: "320px", height: "180px", background: "", border: "1px solid white" }}>
                         <div style={{ background: "", display: "flex", flexDirection: "column", flex: 1, borderRight: "1px solid white" }}>
-                            <h3 className = "stats-number" style={{ flex: 1, display: "flex", justifyContent: "center", alignItems: "center", borderBottom: "1px solid white" }}>5</h3>
-                            <h3 className = "stats-number" style={{ flex: 1, display: "flex", justifyContent: "center", alignItems: "center" }}>12</h3>
+                            <h3 className = "stats-number" style={{ flex: 1, display: "flex", justifyContent: "center", alignItems: "center", borderBottom: "1px solid white" }}>{ stats.FT }</h3>
+                            <h3 className = "stats-number" style={{ flex: 1, display: "flex", justifyContent: "center", alignItems: "center" }}>{ stats.FTA }</h3>
                         </div>
                         <div style={{ position : "relative", flex: 1, display: "flex", justifyContent: "center", alignItems: "center" }}>
                             <div style = { { display : "flex", flexDirection : "row", alignItems : "flex-end"} }>
-                                <h3 className = "stats-number" style = { { fontSize : "100px" } }>42</h3>
+                                <h3 className = "stats-number" style = { { fontSize : "100px" } }>{ stats.FTP }</h3>
                                 <p className = "percentage">%</p>
                             </div>
                             <span 
                                 ref={(el) => spanRefs.current[2] = el}
-                                data-target-height="42%"
+                                data-target-height = {`${stats.FTP}%`}
                                 style = { { bottom : "0", position : "absolute", width : "100%", height : "0%", background: "white", mixBlendMode: "difference", }}
                             />
                         </div>
@@ -195,15 +224,15 @@ export default function Player(){
             {/* GAME INFO */}
             <h1>Game</h1>
             <div style={{ display: "flex", flexDirection: "row", gap: "16px", alignItems : "center" }}>
-                <p>CC vs CGB</p>
+                <p>SCP vs EL</p>
                 <div style={{ display: "flex", flexDirection: "row", gap: "4px", alignItems: "center" }}>
                     <Scoreboard style={{ fontSize: "14px", color: "white" }} />
-                    <p className="meta">62 : 27</p>
+                    <p className="meta">57 : 61</p>
                 </div>
 
                 <div style={{ display: "flex", flexDirection: "row", gap: "4px", alignItems: "center" }}>
                     <EventAvailable style={{ fontSize: "14px", color: "white" }} />
-                    <p className="meta">Aug 6, 2025</p>
+                    <p className="meta">Aug 20, 2025</p>
                 </div>
 
                 <div style={{ display: "flex", flexDirection: "row", gap: "4px", alignItems: "center" }}>
